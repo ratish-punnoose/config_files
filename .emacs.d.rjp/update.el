5d9b1610813a1555b54c4e652c3d418eb9c65f26
Ratish Punnoose <rjpunno@sandia.gov>
2020-05-22 18:39:59 -0700

    .emacs: Updated build.  Added install/update
    install installs missing packages
    update force updates packages to new version

--- .emacs.d.rjp/update.el
+++ .emacs.d.rjp/update.el
@@ -18,22 +18,27 @@
   (interactive)
   (if (file-accessible-directory-p dirname)
       (append (list dirname) (seq-filter  'file-directory-p
-					  (nthcdr 2 ;; Remove ".", ".."
-						  (directory-files dirname t))))
+                                          (nthcdr 2 ;; Remove ".", ".."
+                                                  (directory-files dirname t))))
     nil))
 (setq load-path (append load-path
-			(rjp/get-dir-and-subdirs
-			 (expand-file-name "elpa/"
-					   user-emacs-directory))
-			 (rjp/get-dir-and-subdirs
-			  (expand-file-name "lisp/"
-					    user-emacs-directory))
-			))
+                        (rjp/get-dir-and-subdirs
+                         (expand-file-name "elpa/"
+                                           user-emacs-directory))
+                         (rjp/get-dir-and-subdirs
+                          (expand-file-name "lisp/"
+                                            user-emacs-directory))
+                        ))
 (require 'use-package-ensure)
 (setq use-package-always-ensure t)
 (setq package-load-list '(all
-			  ))
+                          ))
 (setq rjp/emacs-in-update t)
 (load-file "init.el")
+(if (string= (nth 1 argv) "force")
+    (use-package auto-package-update
+      :config
+      (setq auto-package-update-delete-old-versions t)
+      (auto-package-update-now)))
 ;;; update.el ends here
 

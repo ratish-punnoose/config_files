c413afc52ed605b44d308dcadbb69ccac9aca466
Ratish Punnoose <rjpunno@sandia.gov>
2026-02-15 21:33:47 -0800

    .emacs: Fix for loadpath to prioritize custom packages

--- .emacs.d.rjp/early-init.el
+++ .emacs.d.rjp/early-init.el
@@ -1,6 +1,10 @@
 (unless (boundp 'rjp/emacs-in-update)
-  ;;  To make magit work property, this was changed from nil to t.
-  (setq package-enable-at-startup t)
+  ;; Package is disabled unless we are in update mode.
+  ;; Even the quickstart export is not used.
+  ;; package-enable-at-startup was changed from nil to t to fix magit.
+  ;; But this was a symptom, the cause was due to default packages being loaded
+  ;; earlier than custom additions.  That is fixed so this is now reverted.
+  (setq package-enable-at-startup nil)
   (setq package-load-list nil)
   (setq package-quickstart t)
   )
